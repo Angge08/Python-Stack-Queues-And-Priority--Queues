4bdb33fb82ed66ade11702d6414eb8195fd8cbc4
Angge08 <anggedelmendo08@gmail.com>
2022-12-16 19:45:38 +0800

    def __len__

--- queue4_updated.py
+++ queue4_updated.py
@@ -3,4 +3,10 @@ from heapq import heappop, heappush
 
 class Queue:
     def __init__(self, *elements):
-    self._elements = deque(elements)
+     self._elements = deque(elements)
+
+    def __len__(self):
+        return len(self._elements)
+    def __iter__(self):
+        while len(self) > 0:
+            yield self.dequeue()
