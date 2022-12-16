4a8320bc483a75486c5ff1b56011a6e746684839
Angge08 <anggedelmendo08@gmail.com>
2022-12-16 18:36:09 +0800

    while len

--- queue3_updated.py
+++ queue3_updated.py
@@ -5,3 +5,6 @@ class Queue:
         self._elements = deque(elements)
     def __len__(self):
         return len(self._elements)
+    def __iter__(self):
+        while len(self) > 0:
+            yield self.dequeue()
