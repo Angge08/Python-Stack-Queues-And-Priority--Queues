9f7c04d6044f40baaacab1a812f56631ccb21752
Angge08 <anggedelmendo08@gmail.com>
2022-12-16 21:31:55 +0800

    Class Iterable Mixin

--- queue7_updated.py
+++ queue7_updated.py
@@ -2,3 +2,10 @@ from collections import deque
 from heapq import heappop, heappush
 from itertools import count
 
+class IterableMixin:
+    def __len__(self):
+        return len(self._elements)
+
+    def __iter__(self):
+        while len(self) > 0:
+            yield self.dequeue()
