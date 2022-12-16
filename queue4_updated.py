235de434e0d5b72d10a279f744ad63ff4fde66ba
Angge08 <anggedelmendo08@gmail.com>
2022-12-16 19:43:46 +0800

    self._elements

--- queue4_updated.py
+++ queue4_updated.py
@@ -2,6 +2,5 @@ from collections import deque
 from heapq import heappop, heappush
 
 class Queue:
-    def
-    __init__(self, *elements):
+    def __init__(self, *elements):
     self._elements = deque(elements)
